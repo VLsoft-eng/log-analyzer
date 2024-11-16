146ad298d93425724ebc7afae0588047e459f69b
VLsoft-eng <deeeaddrop@gmail.com>
2024-11-17 02:58:09 +0700

    build: add log processing calling in main

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,9 +1,25 @@
 package backend.academy;
 
+import backend.academy.jcommander.Args;
+import com.beust.jcommander.JCommander;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Main {
     public static void main(String[] args) {
+        Args params = new Args();
+
+        try {
+            JCommander.newBuilder()
+                .addObject(params)
+                .build()
+                .parse(args);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return;
+        }
+
+        LogProcessingManager logProcessingManager = new LogProcessingManager(System.out);
+        logProcessingManager.process(params);
     }
 }
